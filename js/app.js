1208247c4a8fbd4f72eec7d05d85f4f3392487f0
Ebere Uzodufa <ebereuzodufa@gmail.com>
2019-04-28 19:41:04 +0100

    Create function createPropertyHTML which is used to create property layout HTML
    It created the divs, ps and image required to create properties and link required.

--- js/app.js
+++ js/app.js
@@ -103,13 +103,135 @@ const fetchProperties = () =>{
 	// console.log("properties", properties);
 }
 
+//Generate HTML by first, getting each property
 const generatePropertyHTML = () =>{
 	const propertyArticle = document.querySelector('article.article-feartured-properties');
 	if (propertyArticle) {
 		properties.forEach(property =>{
-			console.log(property.id, property);
+			// console.log(property.id, property);
+			propertyArticle.append(createPropertyHTML(property));
 		});
 	}
+}
+
+//Create HTML for Property
+const createPropertyHTML = (property) =>{
+	const {
+		id,
+		name,
+		type,
+		address,
+		images,
+		writeUp,
+		bottomWriteUp,
+		price,
+		propertySize,
+		propertyTitle,
+		propertyOfferType,
+		propertyNeighborhood,
+		propertyFeatures,
+		map,
+		videoLink
+	} = property;
+
+	// console.log(id,id);
+	// console.log(id,name);
+	// console.log(id,type);
+	// console.log(id,address);
+	// console.log(id,images);
+	// console.log(id,writeUp);
+	// console.log(id,bottomWriteUp);
+	// console.log(id,price);
+	// console.log(id,propertySize);
+	// console.log(id,propertyTitle);
+	// console.log(id,propertyOfferType);
+	// console.log(id,propertyNeighborhood);
+	// console.log(id,propertyFeatures);
+	// console.log(id,map);
+	// console.log(id,videoLink);
+
+	// parent container <div class="container property">
+	const divConProperty = document.createElement('div');
+	divConProperty.classList.add('container');
+	divConProperty.classList.add('property');
+
+	// image from images. For now we are using demo
+	const imgProperty = document.createElement('img');
+	imgProperty.classList.add('property-image');
+	imgProperty.src = "images/property-test.jpg";
+	imgProperty.alt = "Image of property, ";
+
+	//Append image (imgProperty) to divConProperty
+	divConProperty.append(imgProperty);
+
+	//paragraph of propery-name
+	const paraPropName = document.createElement('p');
+	paraPropName.classList.add('property-name');
+	paraPropName.innerHTML = name;
+	//Append paragraph prop-name to divConProperty
+	divConProperty.append(paraPropName);
+
+	//paragraph of propery-location
+	const paraPropLoc = document.createElement('p');
+	paraPropLoc.classList.add('property-location');
+	paraPropLoc.innerHTML = address;
+	//Append paragraph prop-location to divConProperty
+	divConProperty.append(paraPropLoc);
+
+	//div container for money
+	const divConMoney = document.createElement('div');
+	divConMoney.classList.add('container');
+
+	//create fontawesome i
+	const iMoney = document.createElement('i');
+	iMoney.classList.add('fas');
+	iMoney.classList.add('fa-money-bill');
+	iMoney.classList.add('property');
+	//Append iMoney to divConMoney
+	divConMoney.append(iMoney);
+
+	//paragraph of propery-price
+	const paraPropPrice = document.createElement('p');
+	paraPropPrice.classList.add('property-price');
+	paraPropPrice.innerHTML = price.mainPrice;
+	//Append paragraph prop-price to divConMoney
+	divConMoney.append(paraPropPrice);
+
+	//Append divConMoney to divConProperty
+	divConProperty.append(divConMoney);
+
+	//div container for property size
+	const divConSize = document.createElement('div');
+	divConSize.classList.add('container');
+
+	//create fontawesome i
+	const iSize = document.createElement('i');
+	iSize.classList.add('fas');
+	iSize.classList.add('fa-box-open');
+	iSize.classList.add('property');
+	//Append iSize to divConSize
+	divConSize.append(iSize);
+
+	//paragraph of propery-price
+	const paraPropSize = document.createElement('p');
+	paraPropSize.classList.add('property-size');
+	paraPropSize.innerHTML = propertySize;
+	//Append paragraph prop-price to divConSize
+	divConSize.append(paraPropSize);
+
+	//Append divConSize to divConProperty
+	divConProperty.append(divConSize);
+
+	//Create link
+	const aLink = document.createElement('a');
+	aLink.role = "button";
+	aLink.classList.add('property-link');
+	//Append aLink to divConProperty
+	divConProperty.append(aLink);
+
+	// console.log(id, divConProperty);
+
+	return divConProperty;
 }
 
 // On application start, perform these
