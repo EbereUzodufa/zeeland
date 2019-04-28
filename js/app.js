a5f10f3ed106c850f2fc56f0d0a42c8a0d046250
Ebere Uzodufa <ebereuzodufa@gmail.com>
2019-04-28 03:19:50 +0100

    Toggle menu on mobile
    On mobile, toggle btn is visibile. I create a startApp() which calls necessary function when the app lis launched.
    
    currentYear gets the current year
    setOverlay helps set overlay and body CSS
    toogleMenuBtn helps toggle the menu display
    SetAriaBasedOnScreen helps monitor ARIA of overlay in mobile and Desktop

---
+++ js/app.js
@@ -0,0 +1,70 @@
+// Global Declaration
+const btnToggle = document.getElementById('btnMobileToggle');
+const body = document.body;
+
+const overlay = document.querySelector('div.overlay');
+
+//Set aria-hidden property
+SetAriaHidden = (val) =>{
+	overlay.setAttribute('aria-hidden', val);
+}
+
+// Get current year
+const currentYear = () => {
+	let d = new Date();
+	let year = d.getFullYear();
+	document.getElementById('currentYear').innerHTML = year;
+}
+
+// Set overlay
+const setOverlay = (value) =>{
+	let setValue = value;
+	// console.log(setValue);
+	if (setValue == true){
+		overlay.style.width = '100%';
+		overlay.scrollTop = 0;
+		body.classList.add('noScroll');
+		SetAriaHidden('false');
+	} else{
+		overlay.style.width = '0%';
+		body.classList.remove('noScroll');
+		SetAriaHidden('true');
+	}
+};
+
+//Toggle Menu Button
+const toogleMenuBtn = () =>{
+	// Toggle Button
+	const iAwesome = document.querySelector('i.fa.fa-bars');
+	let giveOverlay = false;
+	btnToggle.addEventListener('click', function() {
+		if (btnToggle.style.backgroundColor == '' || btnToggle.style.backgroundColor == 'transparent') {
+			btnToggle.style.backgroundColor = "#bf9f62";
+			iAwesome.style.color = '#fff';
+			giveOverlay = true;
+		} else{
+			btnToggle.style.backgroundColor = "transparent";
+			iAwesome.style.color = '#291670';
+			giveOverlay = false;
+		}
+		setOverlay(giveOverlay);
+	});
+}
+
+//Change ARIA based on screen width
+const SetAriaBAsedOnScreen = () =>{
+	const screenWidth = screen.width;
+	if (screenWidth >= 1024) {
+		SetAriaHidden('false');
+	} else {
+		SetAriaHidden('true');
+	}
+}
+
+// On application start, perform these
+const startApp = () => {
+	toogleMenuBtn(); //Enable Toggle Menu
+	SetAriaBAsedOnScreen();
+};
+
+startApp();
